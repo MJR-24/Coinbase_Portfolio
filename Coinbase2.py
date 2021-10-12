39a05e3f94d5869b85eb9afe5c6f8b0541cf281f
Mark J Ryan <90434341+MJR-24@users.noreply.github.com>
2021-10-12 18:17:23 -0400

    Add files via upload

--- Coinbase2.py
+++ Coinbase2.py
@@ -1,40 +1,53 @@
 """
-Coinbase (https://www.coinbase.com/) is creating a new portfolio manager. 
-They have hired you to create a prototype that manages a set of currencies in a portfolio. 
-This system will include at least two different classes that you design. 
-Minimum specifications:
-    1.	Create some currency objects, which keep track of individual investments, their number of shares and their current price. 
-    2.	Create some portfolio objects, which keeps track of a group of currency investments.  
-        The portfolio should enable printing its current holdings, as well as the total value of their portfolio. 
-        You should also include other things as you see fit.
-    3.	Allow the user to sell currencies (reduce the shares), purchase currencies items (increase the shares), and 
-    4.	Consider adding other functionality of your choosing (what else might they want to do?).
-    5.	Create a menu so that the user can interact with the system 
-        (e.g. to order an item press 1, to restock an item press 2...) Keep showing the menu until the user says stop (this will be done in a loop).
-    6.	Print out the inventory of all items after all of the changes from the user, include how many of each one is currently available and print totals.
+    Coinbase (https://www.coinbase.com/) is creating a new portfolio manager. 
+    They have hired you to create a prototype that manages a set of currencies in a portfolio. 
+    This system will include at least two different classes that you design. 
 """
 
 class Portfolio:
-# Attributes: CURRENCY Investments, Currency Holdings, Portfolio Total Value
+    """ Portfolio class will:
+            Keep track of Currency Investments.
+            Print current Holdings.
+            Calculate and print Total Portfolio Value.
+    """
     def __init__(self):
         self.holdings = []
+        self.holdingsCost = []
         self.totalVal = 0
 
-    def addHolding(self, newHolding):
+    def addHolding(self):
+        # Adds Holdings in a loop until the user says stop.
         h = input("What are your holdings? ")
         self.holdings.append(h)
-        self.holdings.append(newHolding)
 
-    def printHoldings(self):
-        print(self.holdings)
+        p = input("What is the price of that holding? ")
+        self.totalVal = self.totalVal + int(p)
+        self.holdingsCost.append(f'${p}')
 
-     
+        ans = input("Add another holding? ")
 
-    
+        while ans == 'yes' or ans == 'Yes':
+            if ans == 'yes' or ans == 'Yes':
+                h = input("What are your holdings? ")
+                self.holdings.append(h)
+                p = input("What is the price of that holding? ")
+                self.totalVal = self.totalVal + int(p)
+                self.holdingsCost.append(f'${p}')
+                ans = input("Add another holding? ")
+            else:
+                pass
 
+    def findTotal(self):
+        pass
 
 class Currency:
-# Attributes: Investments, Number of Shares, Current Price
+    """ Currency class will:
+            Keep track of Individual Investments
+            Keep track of Number of Shares
+            Keep track of Current Price of Shares
+            Allow user to Sell currencies (Reduce Shares)
+            Allow user to Buy currencies (Increase Shares)
+    """
     def __init__(self, investments, shares, price):
         self.investments = investments
         self.shares = shares
@@ -46,7 +59,16 @@ class Currency:
     def sell(self, sharesS):
         self.shares.append(sharesS)
 
+# TODO: Create a menu for user to interact with system.
+        # Keep showing menu until user says to stop.
+
+# TODO: Print inventory of all items after all changes from user. 
+        # Including: How many of each one is currently available &
+                    # Print the totals. 
 ######################################################################
 
 myPort = Portfolio()
+myPort.addHolding()
 
+print(myPort.holdings)
+print(myPort.holdingsCost)
